182092cee7faa2aa5e0aa0d1783e75461bd481cc
Valentin Postnov <valpostnov@gmail.com>
2016-06-10 18:06:57 +0300

    Initial commit

---
+++ app/src/main/java/com/postnov/android/intechtestapp/ListArtistActivity.java
@@ -0,0 +1,13 @@
+package com.postnov.android.intechtestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListArtistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_artist);
+    }
+}
